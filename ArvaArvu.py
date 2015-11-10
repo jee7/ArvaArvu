fca8f642a07bae4108bb8ec4816a21ff04a5b38a
jee7 <raimondtunnel@hotmail.com>
2015-11-10 02:32:29 +0200

    Muutsin koodi paremaks.

--- ArvaArvu.py
+++ ArvaArvu.py
@@ -6,19 +6,17 @@ print("Tere! Mis su nimi on?")
 nimi = input()
 print("Hei, "+ nimi+ ", Arva ära millist arvu 10 ja 30 vahel ma mõtlen?")
 arvamus = int(input())
-loendur = 1
+loendur = 0
 
-while arvamus != arv :
+while arvamus != arv or loendur < 10:
     if arv > arvamus:
         print ("Liiga väike! Paku suuremat arvu!")
     elif arv < arvamus:
         print ("Liiga suur! Paku väiksemat arvu!")
     arvamus = int(input())
     loendur = loendur + 1
-    if loendur > 10:
-        break
 
-if loendur <= 10:
+if loendur <= 10 and arvamus == arv:
     print ("Tubli "+nimi+ "! Sa arvasid mu arvu", loendur, "korraga." )
 else :
     print ("Viiest arvamisest ei piisanud! Äkki tahad taktikat muuta?")
